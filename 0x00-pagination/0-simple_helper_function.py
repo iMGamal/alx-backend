67c67814062b253629786d7e5a37547b12af96fe
iMGamal <mohamed.gamal126027@gmail.com>
2024-12-07 14:44:08 +0000

    Task 0 v3 documentation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
+"""There are no modules to be imported."""
+
+
 def index_range(page, page_size):
+    """Return a tuple that contains first and last indices of a page."""
+
     first = (page - 1) * page_size
     last = (page * page_size)
     return (first, last)
